9821fe9a77599eb1e4dd302df1a7915984b2a0dd
Pablo Hitos <pablo_hitos@hotmail.com>
2018-11-30 00:05:11 +0100

    Este es el primer commit

---
+++ src/Saludo.java
@@ -0,0 +1,31 @@
+import java.math.*;
+import java.time.LocalDateTime;
+
+
+public class Saludo {
+
+	public static void main(String[] args) {
+
+		
+		/**
+		 * ArrayList<Triangulo> trians = new ArrayList<Triangulo>(); 
+		trians.add(new Triangulo(1, 2, 3)); 
+		trians.add(new Triangulo(1, 1, 2)); 
+		trians.add(new Triangulo(1, 1, 1));
+		
+		LocalDateTime fecha = LocalDateTime.now();
+		System.out.println("Fecha del sistema: " + fecha);
+		System.out.println("Mes: " + fecha.getMonth());
+		System.out.println("Dia: " + fecha.getDayOfMonth());
+		
+		trians.add(1, new Triangulo(7, 8, 9));
+		
+		System.out.println(Integer.MIN_VALUE);
+		 */
+		
+			
+		// TODO Auto-generated method stub
+
+	}
+
+}
